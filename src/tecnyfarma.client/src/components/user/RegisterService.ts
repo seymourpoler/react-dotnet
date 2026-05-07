dedb67ae56c84e25e7092e0067416511128f6342
Luis Rodríguez <luis.rodriguez@leanmind.es>
2026-05-07 17:00:22 +0200

    refactor register function in RegisterService for improved readability

--- src/tecnyfarma.client/src/components/user/RegisterService.ts
+++ src/tecnyfarma.client/src/components/user/RegisterService.ts
@@ -3,10 +3,11 @@
       password: string;
     };
 export async function register(request: userRegisterRequest) : Promise<Response> {
-    const response = await fetch('/api/v0/register', {
+    const url = '/api/v0/register';
+    
+    return await fetch(url, {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ email: request.email, password: request.password })
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({email: request.email, password: request.password})
     });
-    return response;
 }
