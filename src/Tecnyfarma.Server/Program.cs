8230b906c2220e51c9dc8fea923d02fe40d9d6c4
Luis Rodríguez <luis.rodriguez@leanmind.es>
2026-09-04 11:14:50 +0200

    fix: authentication error

--- src/Tecnyfarma.Server/Program.cs
+++ src/Tecnyfarma.Server/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 using Tecnyfarma.Server.User.Infrastructure;
 using Tecnyfarma.Server.User.Infrastructure.DataBase;
@@ -11,6 +12,14 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
+        builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddCookie(options =>
+            {
+                options.LoginPath = "/api/v0/users/login";
+                options.Cookie.HttpOnly = true;
+                options.Cookie.SameSite = SameSiteMode.Strict;
+                options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+            });
         builder.Services.AddControllers();
         builder.Services.AddUserDependencies(builder.Configuration);
 
@@ -20,6 +29,7 @@ public class Program
 
         // Configure the HTTP request pipeline.
         app.UseHttpsRedirection();
+        app.UseAuthentication();
         app.UseAuthorization();
         app.MapControllers();
         app.MapFallbackToFile("/index.html");
