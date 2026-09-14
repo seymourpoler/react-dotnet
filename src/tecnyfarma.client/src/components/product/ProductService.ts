b9126b7751ba4bd1d25a03a759c1f399ce57f3aa
Luis Rodríguez <luis.rodriguez@leanmind.es>
2026-09-14 19:48:12 +0200

    refactor: format code

--- src/tecnyfarma.client/src/components/product/ProductService.ts
+++ src/tecnyfarma.client/src/components/product/ProductService.ts
@@ -7,6 +7,7 @@
 
 export async function find(): Promise<Response> {
     const url = "/api/v0/products";
+    
     return await fetch(url, {
         method: 'GET',
         headers: {'Content-Type': 'application/json'}
